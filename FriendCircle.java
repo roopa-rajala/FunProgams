c305caa5173f36a26bac7a5032e80ccc7638cc21
roopa-rajala <roopaprasad151@gmail.com>
2018-08-09 16:52:22 -0500

    Create FriendCircle.java
    Input:
    4
    YYNN
    YYYN
    NYYN
    NNNY
    
    output:
    2

---
+++ FriendCircle.java
@@ -0,0 +1,26 @@
+static int friendCircles(List<String> friends) {
+        // can use dfs for this
+       char[][] arrOfFriend=new char[friends.size()][friends.size()];
+         int i=0;
+          for(String friend:friends){
+            arrOfFriend[i]= friend.toCharArray();
+              i++;
+        }
+        int count=0;
+        int[] visited=new int[friends.size()];
+        for(int row=0;row<friends.size();row++){
+        if(visited[row]==0){
+            dfs(arrOfFriend,visited,row);
+            count++;
+        }
+        }
+        return count;
+    }
+static void dfs(char[][] arrOfFriend,int[] visited,int position){
+    for(int col=0;col<arrOfFriend.length;col++){
+        if(arrOfFriend[position][col]=='Y' && visited[col]==0){
+            visited[col]=1;
+            dfs(arrOfFriend,visited,col);
+        }
+    }
+}
